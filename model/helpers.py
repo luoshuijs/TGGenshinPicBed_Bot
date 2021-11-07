e9781905eb2953b27f619ee1c1f5b7db1efe6519
洛水.山岭居室 <admin@luotianyi.one>
2021-11-07 16:11:25 +0800

    Fix bud.

--- model/helpers.py
+++ model/helpers.py
@@ -15,8 +15,9 @@ def parse_artwork_push_data(
         artwork_push_data.is_error = True
         artwork_push_data.message = error_message
         if status_code == 1:
-            status_code = 0
-        artwork_push_data.status_code = status_code
+            artwork_push_data.status_code = 0
+        else:
+            artwork_push_data.status_code = status_code
     artwork_push_data.artwork_info = artwork_info
     artwork_push_data.artwork_image = artwork_image
     artwork_push_data.count = count
@@ -34,8 +35,9 @@ def parse_artwork_audit_data(
         artwork_audit_data.is_error = True
         artwork_audit_data.message = error_message
         if status_code == 1:
-            status_code = 0
-        artwork_audit_data.status_code = status_code
+            artwork_audit_data.status_code = 0
+        else:
+            artwork_audit_data.status_code = status_code
     artwork_audit_data.artwork_info = artwork_info
     artwork_audit_data.artwork_image = artwork_image
     artwork_audit_data.artwork_audit = artwork_audit
@@ -52,8 +54,9 @@ def parse_artwork_data(
         artwork_data.is_error = True
         artwork_data.message = error_message
         if status_code == 1:
-            status_code = 0
-        artwork_data.status_code = status_code
+            artwork_data.status_code = 0
+        else:
+            artwork_data.status_code = status_code
     artwork_data.artwork_info = artwork_info
     artwork_data.artwork_image = artwork_image
     return artwork_data
