c6fb62c323c889b127a63684e276e2a51311006e
洛水.山岭居室 <admin@luotianyi.one>
2021-12-07 16:28:41 +0800

    Fix a bug.
    ```
    Traceback
    Can't parse entities: character '#' is reserved and must be escaped with the preceding '\'
    ```
    
    The `escape_markdown` default `version` value is 1.
    
    But `parse_mode` is `MARKDOWN_V2`.
    
    Use the `escape_markdown` to set `version` value to `2`.

--- plugins/push.py
+++ plugins/push.py
@@ -99,8 +99,8 @@ class PushHandler:
                 caption = "Title %s   \n" \
                           "Tags %s   \n" \
                           "From [%s](%s)" % (
-                              escape_markdown(artwork_info.title),
-                              escape_markdown(artwork_info.GetStringTags(filter_character_tags=True)),
+                              escape_markdown(artwork_info.title, version=2),
+                              escape_markdown(artwork_info.GetStringTags(filter_character_tags=True), version=2),
                               artwork_info.site_name,
                               artwork_info.origin_url
                           )
