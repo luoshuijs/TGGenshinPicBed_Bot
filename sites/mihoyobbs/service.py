5aebd45bd298857d7a041b93bc2a04d2f6af7a50
洛水.山岭居室 <luoshuijs@foxmail.com>
2022-03-08 20:52:04 +0800

    更新service代码

--- sites/mihoyobbs/service.py
+++ sites/mihoyobbs/service.py
@@ -1,6 +1,6 @@
-from typing import Tuple, Iterable, Optional
+from typing import List
 
-from model.artwork import ArtworkInfo, ArtworkImage
+from model.artwork import ArtworkInfo, ArtworkImage, AuditType
 from model.containers import ArtworkData
 from model.helpers import parse_artwork_data
 from sites import listener
@@ -18,8 +18,8 @@ class MihoyobbsService:
         temp_artwork_info = self.api.get_artwork_info(post_id)
         if temp_artwork_info is None:
             return parse_artwork_data(error_message="请求错误")
-        artwork_image = self.api.get_images_by_artid(post_id)
-        artwork_info = temp_artwork_info.GetArtworkInfo()
+        artwork_image = self.api.get_images_by_artid(temp_artwork_info)
+        artwork_info = temp_artwork_info.results.GetArtworkInfo()
         return parse_artwork_data(artwork_info, artwork_image)
 
     def contribute_start(self, post_id: int) -> ArtworkData:
@@ -29,9 +29,28 @@ class MihoyobbsService:
         temp_artwork_info = self.api.get_artwork_info(post_id)
         if temp_artwork_info is None:
             return parse_artwork_data(error_message="已经存在数据库")
-        artwork_image = self.api.get_images_by_artid(post_id)
-        artwork_info = temp_artwork_info.GetArtworkInfo()
+        artwork_image = self.api.get_images_by_artid(temp_artwork_info)
+        artwork_info = temp_artwork_info.results.GetArtworkInfo()
         return parse_artwork_data(artwork_info, artwork_image)
 
     def contribute(self, artwork_info: ArtworkInfo):
         self.repository.save_art_one(artwork_info.info)
+
+    def get_art_for_audit(self, audit_type: AuditType = AuditType.SFW) -> List[ArtworkInfo]:
+        art_list = self.repository.get_art_for_audit()
+        art_info_list = []
+        for art_info in art_list:
+            info = self.repository.get_art_by_artid(art_info.connection_id)
+            if info is not None:
+                if audit_type.SFW == audit_type:
+                    # 国内有啥涩涩的？
+                    art_info_list.append(info.GetArtworkInfo())
+        return art_info_list
+
+    def get_art_for_push(self, audit_type: AuditType = AuditType.SFW) -> List[ArtworkInfo]:
+        art_list = self.repository.get_art_for_push(audit_type)
+        art_info_list = []
+        for art_info in art_list:
+            info = self.repository.get_art_by_artid(art_info.connection_id)
+            art_info_list.append(info.GetArtworkInfo())
+        return art_info_list
