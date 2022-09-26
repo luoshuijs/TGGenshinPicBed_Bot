ca1925953b7104dfab5390f27cb5478e3a609153
洛水居室 <luoshuijs@outlook.com>
2022-09-26 15:56:30 +0800

    更新错误通知

--- plugins/errorhandler.py
+++ plugins/errorhandler.py
@@ -1,48 +1,77 @@
-import html
+import json
+import os
+import time
 import traceback
-import ujson
-from telegram import Update, ParseMode
+
+from telegram import Update, ParseMode, ReplyKeyboardRemove
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext
+
 from config import config
 from logger import Log
 
+notice_chat_id = config.TELEGRAM["channel"]["LOG"]["char_id"]
+current_dir = os.getcwd()
+logs_dir = os.path.join(current_dir, "logs")
+if not os.path.exists(logs_dir):
+    os.mkdir(logs_dir)
+report_dir = os.path.join(current_dir, "report")
+if not os.path.exists(report_dir):
+    os.mkdir(report_dir)
+
 
 def error_handler(update: object, context: CallbackContext) -> None:
-    """
-    记录错误并发送消息通知开发人员。
-    Log the error and send a telegram message to notify the developer.
-    """
-    Log.error(msg="处理函数时发生异常:", exc_info=context.error)
+    """记录错误并发送消息通知开发人员。 logger the error and send a telegram message to notify the developer."""
+
+    Log.error("处理函数时发生异常")
+
+    if notice_chat_id is None:
+        return
 
     tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    tb_string = ''.join(tb_list)
+    tb_string = "".join(tb_list)
 
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    message_1 = (
-        f'<b>处理函数时发生异常</b> \n'
-        f'Exception while handling an update \n'
-        f'<pre>update = {html.escape(ujson.dumps(update_str, indent=2, ensure_ascii=False))}'
-        '</pre>\n\n'
-        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
-        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+
+    error_text = (
+        f"-----Exception while handling an update-----\n"
+        f"update = {json.dumps(update_str, indent=2, ensure_ascii=False)}\n"
+        f"context.chat_data = {str(context.chat_data)}\n"
+        f"context.user_data = {str(context.user_data)}\n"
+        "\n"
+        "-----Traceback info-----\n"
+        f"{tb_string}"
     )
-    message_2 = (
-        f'<pre>{html.escape(tb_string)}</pre>'
-    )
-    channel_id = config.TELEGRAM["channel"]["LOG"]["char_id"]
+    file_name = f"error_{update.update_id if isinstance(update, Update) else int(time.time())}.txt"
+    log_file = os.path.join(report_dir, file_name)
+    try:
+        with open(log_file, mode='w+', encoding='utf-8') as f:
+            f.write(error_text)
+    except Exception:
+        Log.error("保存日记失败")
     try:
         if 'make sure that only one bot instance is running' in tb_string:
             Log.error("其他机器人在运行，请停止！")
             return
-        context.bot.send_message(chat_id=channel_id, text=message_1, parse_mode=ParseMode.HTML)
-        context.bot.send_message(chat_id=channel_id, text=message_2, parse_mode=ParseMode.HTML)
-    except BadRequest as exc:
-        if 'too long' in str(exc):
-            message = (
-                f'<b>处理函数时发生异常，traceback太长导致无法发送，但已写入日志</b> \n'
-                f'<code>{html.escape(str(context.error))}</code>'
-            )
-            context.bot.send_message(chat_id=channel_id, text=message, parse_mode=ParseMode.HTML)
-        else:
-            raise exc
+        if 'Message is not modified' in tb_string:
+            Log.error("消息未修改")
+            return
+        context.bot.send_document(chat_id=notice_chat_id, document=open(log_file, "rb"),
+                                  caption=f"Error: \"{context.error.__class__.__name__}\"")
+    except BadRequest:
+        Log.error("发送日记失败")
+    except FileNotFoundError:
+        Log.error("发送日记失败 文件不存在")
+    effective_user = update.effective_user
+    effective_message = update.effective_message
+    try:
+        if effective_message is not None:
+            chat = effective_message.chat
+            Log.info(f"尝试通知用户 {effective_user.full_name}[{effective_user.id}] "
+                     f"在 {chat.full_name}[{chat.id}]"
+                     f"的 update_id[{update.update_id}] 错误信息")
+            text = "出错了呜呜呜 ~ 派蒙这边发生了点问题无法处理！"
+            context.bot.send_message(effective_message.chat_id, text, reply_markup=ReplyKeyboardRemove(),
+                                     parse_mode=ParseMode.HTML)
+    except BadRequest:
+        Log.error(f"发送 update_id[{update.update_id}] 错误信息失败 错误信息为")
